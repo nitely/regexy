4724ae513cfe3b530a1970653e414aa9d0354b2d
nitely <ecastroborsani@gmail.com>
2017-06-28 04:10:29 -0300

    docstrings for captures

--- regexy/process/captures.py
+++ regexy/process/captures.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 
+"""
+Tools for creating the capturing\
+groups matching results of ``match``\
+and friends
+"""
+
 import collections
+from typing import (
+    Union,
+    Tuple,
+    Optional)
 
 from ..shared import Symbols
 
@@ -16,9 +26,42 @@ Capture = collections.namedtuple('Capture', (
     'prev',
     'index',
     'is_repeated'))
+Capture.__doc__ = """
+    This contains a capture (node)\
+    that stores the matched character.\
+    The end result can be transversed as\
+    a sort of reversed linked-list
+
+    The structure is actually flat since\
+    it can only be transversed in reverse,\
+    but if there were a way to go forward\
+    (which there is not), then the structure\
+    would look like a tree containing all\
+    matched branches
+
+    There is no distinction between a matched\
+    char and start/end group, except the group\
+    must has an index and the repeated flag set
+
+    :ivar str char: matched char
+    :ivar Capture prev: previous captured char
+    :ivar int index: group index
+    :ivar bool is_repeated: whether the group\
+    is repeated (i.e: has ``*``, ``+``, etc) or not
+    :private:
+"""
 
 
-def capture(char, prev, index=None, is_repeated=False):
+def capture(
+        char: str,
+        prev: Capture,
+        index: int=None,
+        is_repeated: bool=False) -> Capture:
+    """
+    Build a Capture with some optional params
+
+    :private:
+    """
     return Capture(
         char=char,
         prev=prev,
@@ -26,9 +69,17 @@ def capture(char, prev, index=None, is_repeated=False):
         is_repeated=is_repeated)
 
 
-def _join_reversed(group):
-    if group is None:
-        return None
+def _join_reversed(group: list) -> Union[str, Tuple[str]]:
+    """
+    Reverse-join every match and sub-match
+
+    :param group: a char list of matches\
+    and sub-matches
+    :return: matched groups as strings\
+    or tuple of strings
+    :private:
+    """
+    assert isinstance(group, list)
 
     if not group:
         return ''
@@ -41,7 +92,27 @@ def _join_reversed(group):
         for sub_match in reversed(group))
 
 
-def matched(captured, groups_count):
+MatchedType = Tuple[Union[str, Tuple[str], None]]
+
+
+def matched(captured: Optional[Capture], groups_count: int) -> MatchedType:
+    """
+    Construct the matched strings transversing\
+    given a captured structure
+
+    The passed Capture has the last captured char\
+    and so the sequence is transversed in reverse
+
+    Matches are put in their group index
+
+    Sub-matches (repeating groups) are put\
+    into a nested sequence of their group index
+
+    :param captured: The last capture or None
+    :param groups_count: number of groups
+    :return: matched strings
+    :private:
+    """
     match = collections.defaultdict(lambda: [])
     curr_groups = []
 
@@ -71,5 +142,7 @@ def matched(captured, groups_count):
     assert not curr_groups
 
     return tuple(
-        _join_reversed(match.get(g))
+        _join_reversed(match[g])
+        if g in match
+        else None
         for g in range(groups_count))
